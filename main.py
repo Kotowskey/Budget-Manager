a4c87c95b9c7acb53efc4771248ef07aec1c6caa
Łukasz Kotowski <85959@student.pb.edu.pl>
2024-11-14 20:09:29 +0100

    implementacja Textual

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from controller import BudzetController
+from textual.app import App
+from textual.widgets import Footer, Header
+
+class BudgetApp(App):
+    pass
 
 if __name__ == "__main__":
     kontroler = BudzetController()
-    kontroler.uruchom()
+    BudgetApp().run()
