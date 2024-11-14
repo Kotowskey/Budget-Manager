caab1ecbd301b54d0a032d0b75d36ae86b3437e5
Łukasz Kotowski <85959@student.pb.edu.pl>
2024-11-14 20:25:35 +0100

    Curses

--- controller.py
+++ controller.py
@@ -1,66 +1,89 @@
+import curses
 from model import BudzetModel, Transakcja
-from view import BudzetView
+from curses_view import BudzetCursesView
 
 class BudzetController:
     def __init__(self):
         self.model = BudzetModel()
-        self.view = BudzetView()
+        self.view = BudzetCursesView()
 
     def uruchom(self):
-        while True:
-            self.view.wyswietl_menu()
-            opcja = self.view.pobierz_opcje()
-            if opcja == '1':
-                dane = self.view.pobierz_dane_transakcji()
-                if dane['typ'].lower() == 'wydatek':
-                    if not self.model.sprawdz_limit(dane['kategoria']):
-                        self.view.wyswietl_komunikat("Przekroczono limit budżetowy dla tej kategorii!")
-                        continue
-                transakcja = Transakcja(**dane)
-                self.model.dodaj_transakcje(transakcja)
-                self.view.wyswietl_komunikat("Transakcja dodana.")
-            elif opcja == '2':
-                self.view.wyswietl_transakcje(self.model.transakcje)
-                indeks = self.view.pobierz_indeks_transakcji()
-                if 0 <= indeks < len(self.model.transakcje):
-                    dane = self.view.pobierz_dane_transakcji(edycja=True)
+        try:
+            while True:
+                self.view.wyswietl_menu()
+                opcja = self.view.pobierz_opcje()
+                if opcja == '1':
+                    dane = self.view.pobierz_dane_transakcji()
+                    if dane['typ'].lower() == 'wydatek':
+                        if not self.model.sprawdz_limit(dane['kategoria']):
+                            self.view.wyswietl_komunikat("Przekroczono limit budżetowy dla tej kategorii!")
+                            continue
                     transakcja = Transakcja(**dane)
-                    self.model.edytuj_transakcje(indeks, transakcja)
-                    self.view.wyswietl_komunikat("Transakcja zaktualizowana.")
+                    self.model.dodaj_transakcje(transakcja)
+                    self.view.wyswietl_komunikat("Transakcja dodana.")
+                elif opcja == '2':
+                    self.view.wyswietl_transakcje(self.model.transakcje)
+                    indeks = self.view.pobierz_indeks_transakcji()
+                    if 0 <= indeks < len(self.model.transakcje):
+                        transakcja_oryginalna = self.model.transakcje[indeks]
+                        self.view.wyswietl_komunikat("Wprowadź nowe dane transakcji:")
+                        dane = self.view.pobierz_dane_transakcji(edycja=True)
+                        # Utrzymanie oryginalnego typu, jeśli nie jest edytowany
+                        dane['typ'] = transakcja_oryginalna.typ
+                        if dane['typ'].lower() == 'wydatek':
+                            if not self.model.sprawdz_limit(dane['kategoria']):
+                                self.view.wyswietl_komunikat("Przekroczono limit budżetowy dla tej kategorii!")
+                                continue
+                        transakcja = Transakcja(**dane)
+                        self.model.edytuj_transakcje(indeks, transakcja)
+                        self.view.wyswietl_komunikat("Transakcja zaktualizowana.")
+                    else:
+                        self.view.wyswietl_komunikat("Nieprawidłowy numer transakcji.")
+                elif opcja == '3':
+                    self.view.wyswietl_transakcje(self.model.transakcje)
+                    indeks = self.view.pobierz_indeks_transakcji()
+                    if 0 <= indeks < len(self.model.transakcje):
+                        self.model.usun_transakcje(indeks)
+                        self.view.wyswietl_komunikat("Transakcja usunięta.")
+                    else:
+                        self.view.wyswietl_komunikat("Nieprawidłowy numer transakcji.")
+                elif opcja == '4':
+                    self.view.wyswietl_transakcje(self.model.transakcje)
+                elif opcja == '5':
+                    saldo = self.oblicz_saldo()
+                    self.view.wyswietl_podsumowanie(saldo)
+                elif opcja == '6':
+                    self.model.eksportuj_do_csv()
+                    self.view.potwierdz_eksport()
+                elif opcja == '7':
+                    self.filtruj_transakcje()
+                elif opcja == '8':
+                    self.ustaw_limit_budzetowy()
+                elif opcja == '9':
+                    self.view.wyswietl_komunikat("Do widzenia!")
+                    break
                 else:
-                    self.view.wyswietl_komunikat("Nieprawidłowy numer transakcji.")
-            elif opcja == '3':
-                self.view.wyswietl_transakcje(self.model.transakcje)
-                indeks = self.view.pobierz_indeks_transakcji()
-                if 0 <= indeks < len(self.model.transakcje):
-                    self.model.usun_transakcje(indeks)
-                    self.view.wyswietl_komunikat("Transakcja usunięta.")
-                else:
-                    self.view.wyswietl_komunikat("Nieprawidłowy numer transakcji.")
-            elif opcja == '4':
-                self.view.wyswietl_transakcje(self.model.transakcje)
-            elif opcja == '5':
-                saldo = self.oblicz_saldo()
-                self.view.wyswietl_podsumowanie(saldo)
-            elif opcja == '6':
-                self.model.eksportuj_do_csv()
-                self.view.potwierdz_eksport()
-            elif opcja == '7':
-                self.filtruj_transakcje()
-            elif opcja == '8':
-                self.ustaw_limit_budzetowy()
-            elif opcja == '9':
-                self.view.wyswietl_komunikat("Do widzenia!")
-                break
-            else:
-                self.view.wyswietl_komunikat("Nieprawidłowa opcja. Spróbuj ponownie.")
+                    self.view.wyswietl_komunikat("Nieprawidłowa opcja. Spróbuj ponownie.")
+        finally:
+            self.view.zakoncz()
 
     def oblicz_saldo(self):
-        return self.model.oblicz_saldo()
+        saldo = 0
+        for t in self.model.transakcje:
+            if t.typ.lower() == 'przychód':
+                saldo += t.kwota
+            elif t.typ.lower() == 'wydatek':
+                saldo -= t.kwota
+        return saldo
 
     def filtruj_transakcje(self):
-        start_date = input("Podaj datę początkową (YYYY-MM-DD): ")
-        end_date = input("Podaj datę końcową (YYYY-MM-DD): ")
+        self.view.stdscr.clear()
+        curses.echo()
+        self.view.stdscr.addstr(1, 1, "Podaj datę początkową (YYYY-MM-DD): ")
+        start_date = self.view.stdscr.getstr(1, 40, 10).decode('utf-8')
+        self.view.stdscr.addstr(2, 1, "Podaj datę końcową (YYYY-MM-DD): ")
+        end_date = self.view.stdscr.getstr(2, 40, 10).decode('utf-8')
+        curses.noecho()
         filtrowane = self.model.filtruj_transakcje_po_dacie(start_date, end_date)
         self.view.wyswietl_transakcje(filtrowane)
 
@@ -71,3 +94,7 @@ class BudzetController:
             self.view.potwierdz_ustawienie_limitu(kategoria, limit)
         else:
             self.view.wyswietl_komunikat("Nieprawidłowe dane. Limit nie został ustawiony.")
+
+if __name__ == "__main__":
+    controller = BudzetController()
+    controller.uruchom()
---
+++ curses_view.py
@@ -0,0 +1,176 @@
+# curses_view.py
+import curses
+from datetime import datetime
+
+class BudzetCursesView:
+    def __init__(self):
+        self.stdscr = curses.initscr()
+        curses.curs_set(0)
+        self.stdscr.keypad(True)
+        curses.start_color()
+        curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLUE)
+        self.current_row = 0
+
+    def wyswietl_menu(self):
+        self.stdscr.clear()
+        menu = [
+            'Dodaj transakcję',
+            'Edytuj transakcję',
+            'Usuń transakcję',
+            'Wyświetl transakcje',
+            'Wyświetl podsumowanie',
+            'Eksportuj transakcje do CSV',
+            'Filtruj transakcje według daty',
+            'Ustaw limit budżetowy',
+            'Wyjście'
+        ]
+        self.wyswietl_menu_opcje(menu)
+        self.stdscr.refresh()
+
+    def wyswietl_menu_opcje(self, menu):
+        h, w = self.stdscr.getmaxyx()
+        for idx, row in enumerate(menu):
+            x = w//2 - len(row)//2
+            y = h//2 - len(menu)//2 + idx
+            if idx == self.current_row:
+                self.stdscr.attron(curses.color_pair(1))
+                self.stdscr.addstr(y, x, row)
+                self.stdscr.attroff(curses.color_pair(1))
+            else:
+                self.stdscr.addstr(y, x, row)
+
+    def pobierz_opcje(self):
+        menu_length = 9  # Aktualna liczba opcji w menu
+        while True:
+            self.wyswietl_menu_opcje([
+                'Dodaj transakcję',
+                'Edytuj transakcję',
+                'Usuń transakcję',
+                'Wyświetl transakcje',
+                'Wyświetl podsumowanie',
+                'Eksportuj transakcje do CSV',
+                'Filtruj transakcje według daty',
+                'Ustaw limit budżetowy',
+                'Wyjście'
+            ])
+            key = self.stdscr.getch()
+            if key == curses.KEY_UP and self.current_row > 0:
+                self.current_row -= 1
+            elif key == curses.KEY_DOWN and self.current_row < menu_length - 1:
+                self.current_row += 1
+            elif key == curses.KEY_ENTER or key in [10, 13]:
+                return str(self.current_row + 1)
+            self.stdscr.refresh()
+
+    def pobierz_dane_transakcji(self, edycja=False):
+        curses.echo()
+        self.stdscr.clear()
+        start_row = 1
+        if not edycja:
+            self.stdscr.addstr(start_row, 1, "Typ (przychód/wydatek): ")
+            typ = self.stdscr.getstr(start_row, 25, 20).decode('utf-8')
+            start_row += 1
+        else:
+            typ = None  # Typ nie jest edytowany
+        self.stdscr.addstr(start_row, 1, "Kwota: ")
+        kwota_input = self.stdscr.getstr(start_row, 25, 20).decode('utf-8')
+        try:
+            kwota = float(kwota_input)
+        except ValueError:
+            kwota = 0.0
+        start_row += 1
+        self.stdscr.addstr(start_row, 1, "Kategoria: ")
+        kategoria = self.stdscr.getstr(start_row, 25, 20).decode('utf-8')
+        start_row += 1
+        self.stdscr.addstr(start_row, 1, "Opis (opcjonalnie): ")
+        opis = self.stdscr.getstr(start_row, 25, 50).decode('utf-8')
+        start_row += 1
+        self.stdscr.addstr(start_row, 1, "Data (YYYY-MM-DD) [opcjonalnie]: ")
+        data_input = self.stdscr.getstr(start_row, 35, 10).decode('utf-8')
+        data = data_input if data_input else datetime.now().strftime('%Y-%m-%d')
+        curses.noecho()
+
+        transakcja = {
+            'kwota': kwota,
+            'kategoria': kategoria,
+            'typ': typ if typ else 'przychód',  # Domyślnie 'przychód' jeśli edycja
+            'opis': opis,
+            'data': data
+        }
+        return transakcja
+
+    def wyswietl_transakcje(self, transakcje):
+        self.stdscr.clear()
+        if not transakcje:
+            self.stdscr.addstr(1, 1, "Brak transakcji.")
+            self.stdscr.refresh()
+            self.stdscr.getch()
+            return
+        header = "Nr | Data       | Typ      | Kwota    | Kategoria     | Opis"
+        self.stdscr.addstr(0, 1, header)
+        self.stdscr.addstr(1, 1, "-" * len(header))
+        for i, t in enumerate(transakcje):
+            linia = f"{i}. | {t.data} | {t.typ.capitalize():8} | {t.kwota:9.2f} | {t.kategoria:13} | {t.opis}"
+            try:
+                self.stdscr.addstr(i+2, 1, linia)
+            except curses.error:
+                # Zapobiega błędom, gdy lista jest dłuższa niż wysokość ekranu
+                break
+        self.stdscr.refresh()
+        self.stdscr.getch()
+
+    def wyswietl_podsumowanie(self, saldo):
+        self.stdscr.clear()
+        self.stdscr.addstr(1, 1, f"Aktualne saldo: {saldo:.2f} zł")
+        self.stdscr.refresh()
+        self.stdscr.getch()
+
+    def pobierz_indeks_transakcji(self):
+        curses.echo()
+        self.stdscr.clear()
+        self.stdscr.addstr(1, 1, "Podaj numer transakcji: ")
+        indeks_input = self.stdscr.getstr(1, 25, 5).decode('utf-8')
+        try:
+            indeks = int(indeks_input)
+        except ValueError:
+            indeks = -1  # Nieprawidłowy indeks
+        curses.noecho()
+        return indeks
+
+    def wyswietl_komunikat(self, komunikat):
+        self.stdscr.clear()
+        self.stdscr.addstr(1, 1, komunikat)
+        self.stdscr.refresh()
+        self.stdscr.getch()
+
+    def pobierz_limit(self):
+        curses.echo()
+        self.stdscr.clear()
+        self.stdscr.addstr(1, 1, "Podaj kategorię: ")
+        kategoria = self.stdscr.getstr(1, 20, 20).decode('utf-8')
+        self.stdscr.addstr(2, 1, "Podaj limit miesięczny: ")
+        limit_input = self.stdscr.getstr(2, 25, 20).decode('utf-8')
+        try:
+            limit = float(limit_input)
+        except ValueError:
+            limit = 0.0
+        curses.noecho()
+        return kategoria, limit
+
+    def potwierdz_eksport(self):
+        self.stdscr.clear()
+        self.stdscr.addstr(1, 1, "Transakcje zostały wyeksportowane do pliku 'transakcje.csv'.")
+        self.stdscr.refresh()
+        self.stdscr.getch()
+
+    def potwierdz_ustawienie_limitu(self, kategoria, limit):
+        self.stdscr.clear()
+        self.stdscr.addstr(1, 1, f"Ustawiono limit {limit:.2f} zł dla kategorii '{kategoria}'.")
+        self.stdscr.refresh()
+        self.stdscr.getch()
+
+    def zakoncz(self):
+        curses.nocbreak()
+        self.stdscr.keypad(False)
+        curses.echo()
+        curses.endwin()
