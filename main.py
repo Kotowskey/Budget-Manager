933406448173333d2a492624e208f88f590c4578
Kotowskey <122537577+Kotowskey@users.noreply.github.com>
2024-12-09 17:00:44 +0100

    przygotowanie do gui

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 # main.py
 from controller import BudzetController
+from curses_view import BudzetCursesView
+from gui_view import BudzetGUIView
+
 
 def main():
-    controller = BudzetController()
-    controller.uruchom()
+    print("Wybierz tryb pracy:")
+    print("1. curses (Konsola)")
+    print("2. GUI (Okienkowy)")
+
+    choice = input("Wybór (1/2): ").strip()
+    if choice == "1":
+        controller = BudzetController()
+        controller.uruchom()
+    elif choice == "2":
+        controller = BudzetController()  # Wymagany kontroler
+        gui = BudzetGUIView(controller)
+        gui.run()
+    else:
+        print("Nieprawidłowy wybór. Zamykam program.")
+
 
 if __name__ == "__main__":
     main()
