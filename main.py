6cd03f851b953a922940d149d137131c07264338
Kotowskey <122537577+Kotowskey@users.noreply.github.com>
2025-01-20 11:35:58 +0100

    usunięcie gui(przestarzałe)

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
-import argparse
 from controller import BudzetController
-from gui_view import BudzetGUIView
 from curses_view import BudzetCursesView
 
 def main():
-    parser = argparse.ArgumentParser(description="Aplikacja Budżetowa")
-    parser.add_argument("--interface", choices=["gui", "tui"], default="tui", 
-                        help="Wybierz interfejs: 'gui' (graficzny) lub 'tui' (terminalowy)")
-    args = parser.parse_args()
-
-    controller = BudzetController()
-    view = BudzetGUIView(controller) if args.interface == "gui" else BudzetCursesView()
-    controller.view = view
-    
-    view.run() if args.interface == "gui" else controller.uruchom()
+    try:
+        controller = BudzetController()
+        controller.uruchom()
+    except KeyboardInterrupt:
+        print("\nProgram zakończony przez użytkownika.")
+    except Exception as e:
+        print(f"Wystąpił nieoczekiwany błąd: {e}")
+    finally:
+        if hasattr(controller, 'view'):
+            controller.view.zakoncz()
 
 if __name__ == "__main__":
     main()
